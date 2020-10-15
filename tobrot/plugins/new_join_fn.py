5bf2135ea019553a8c6ee8be805d78237e742c1d
sravanjarvis <68535329+sravanjarvis@users.noreply.github.com>
2020-10-15 21:38:48 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""**Hi sir !!**\n`This is URL Uploader Bot !!`\n\n**Send** `/help` **to see all the commands.\n**""", disable_web_page_preview=True)
+    await message.reply_text("""**Hi sir !!**\n`This is URL Uploader Bot !!`\n\n**Send** `#help` **to see all the commands.\n**""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
