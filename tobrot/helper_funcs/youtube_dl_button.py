1e7dfe3e63ac2c08bf359dd161e1418832fa4e68
sravanjarvis <68535329+sravanjarvis@users.noreply.github.com>
2020-11-19 12:32:26 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | gautamjay52
 
 # the logging things
 import logging
@@ -31,6 +31,12 @@ from tobrot.helper_funcs.upload_to_tg import upload_to_tg, upload_to_gdrive
 async def youtube_dl_call_back(bot, update):
     LOGGER.info(update)
     cb_data = update.data
+    get_cf_name = update.message.caption
+    #LOGGER.info(get_cf_name)
+    cf_name = ""
+    if "|" in get_cf_name:
+        cf_name = get_cf_name.split("|", maxsplit=1)[1]
+        cf_name = cf_name.strip()
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
     #
@@ -39,7 +45,7 @@ async def youtube_dl_call_back(bot, update):
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
-            text="who are you? 🤪🤔🤔🤔",
+            text="I think, You are not the person who requested this YT-DL Link 🤔",
             show_alert=True,
             cache_time=0
         )
@@ -95,12 +101,14 @@ async def youtube_dl_call_back(bot, update):
     #
     tmp_directory_for_each_user = os.path.join(
         DOWNLOAD_LOCATION,
-        str(update.from_user.id)
+        str(update.message.message_id)
     )
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
     download_directory = tmp_directory_for_each_user
+    LOGGER.info(download_directory)
     download_directory = os.path.join(tmp_directory_for_each_user, custom_file_name)
+    LOGGER.info(download_directory)
     command_to_exec = []
     if tg_send_type == "audio":
         command_to_exec = [
@@ -176,19 +184,35 @@ async def youtube_dl_call_back(bot, update):
         )
         user_id = update.from_user.id
         #
-        print(tmp_directory_for_each_user)
-        if os.path.exists('blame_my_knowledge.txt'):
-            for a, b, c in os.walk(tmp_directory_for_each_user):
-                print(a)
-                for d in c:
-                    e = os.path.join(a, d)
-                    print(e)
-                    gaut_am = os.path.basename(e)
-                    print(gaut_am)
-                    liop = subprocess.Popen(["mv", f'{e}', "/app/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    o, e = liop.communicate()
-                    print(o)
-                    print(e)
+        LOGGER.info(tmp_directory_for_each_user)
+        for a, b, c in os.walk(tmp_directory_for_each_user):
+            LOGGER.info(a)
+            for d in c:
+                e = os.path.join(a, d)
+                LOGGER.info(e)
+                gaut_am = os.path.basename(e)
+                LOGGER.info(gaut_am)
+                fi_le = e
+                if cf_name:
+                    fi_le = os.path.join(a, cf_name)
+                    LOGGER.info(fi_le)
+                    os.rename(e, fi_le)
+                    gaut_am = os.path.basename(fi_le)
+                    LOGGER.info(gaut_am)
+                
+        G_DRIVE = False
+        txt = update.message.reply_to_message.text
+        print(txt)
+        g_txt = txt.split()
+        print(g_txt)
+        if len(g_txt) > 1:
+            if g_txt[1] == "gdrive":
+                G_DRIVE = True
+        if G_DRIVE:
+            liop = subprocess.Popen(["mv", f'{fi_le}', "/app/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = liop.communicate()
+            LOGGER.info(out)
+            LOGGER.info(err)
             final_response = await upload_to_gdrive(
                 gaut_am,
                 update.message,
@@ -217,7 +241,6 @@ async def youtube_dl_call_back(bot, update):
         #
         try:
             shutil.rmtree(tmp_directory_for_each_user)
-            os.remove('blame_my_knowledge.txt')
         except:
             pass
         #
